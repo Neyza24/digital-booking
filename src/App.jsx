75b766caa9e51fc8b12cdec6fb6e0e963c486bbf
Neyza <neyzapolo24@gmail.com>
2024-06-10 16:33:59 -0500

    configuration base structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className="text-2xl font-bold underline text-end">
+        Hello world!
+      </h1>
+
+    </>
+  )
+}
+
+export default App
